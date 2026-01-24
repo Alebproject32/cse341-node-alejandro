c903d4bef3dba1af6f668cbcc9763581d2188745
Alejandro Pérez <isaacale25@gmail.com>
2026-01-23 21:29:11 -0400

    Fix a function in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const contactsController = require("../controllers");
+//const contactsController = require("../controllers");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
 router.use("/api-docs", swaggerUi.serve);
 router.get("/api-docs", swaggerUi.setup(swaggerDocument));
-router.get("/professional", contactsController.getProfessionalData);
+//router.get("/professional", contactsController.getProfessionalData);
 router.use("/contacts", require("./contacts"));
 
 module.exports = router;
